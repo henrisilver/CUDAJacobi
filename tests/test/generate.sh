2928177c9732d99fdffd333c6bdf21a5edd9956c
Henrique Silveira <hd2312@columbia.edu>
2016-06-28 22:47:08 -0300

    trying to make generate.sh work

--- tests/test/generate.sh
+++ tests/test/generate.sh
@@ -5,7 +5,7 @@ let i=0
 dimensions=(10 100 1000 10000)
 
 for n in "${dimensions[@]}"
-	echo Generating input$i.in with matrix dimension equal to $n
+	echo Generating inpug$i.in with $n dimension
     ./input_generator $n input$i.in
     let i=i+1
 done
