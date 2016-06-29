7c2e5c10c6ec9cd98ea750f8f085335e11ebf78e
Henrique Silveira <hd2312@columbia.edu>
2016-06-28 22:57:03 -0300

    trying to make generate.sh work

--- tests/run.sh
+++ tests/run.sh
@@ -7,7 +7,7 @@ let i=0
 for filename in test/*.in;
 do
     echo -e "\t$filename"
-    sequential/sequential test/$filename sequential/output$i.out >> stats/sequentialMetrics.mtr
+    sequential/sequential ../test/$filename sequential/output$i.out >> stats/sequentialMetrics.mtr
     let i=i+1
 done
 
@@ -19,7 +19,7 @@ let i=0
 for filename in test/*.in;
 do
     echo -e "\t$filename"
-    parallel/parallel test/$filename parallel/output$i.out >> stats/parallelMetrics.mtr
+    parallel/parallel ../test/$filename parallel/output$i.out >> stats/parallelMetrics.mtr
     let i=i+1
 done
 
